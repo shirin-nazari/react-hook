5a5a34656c1f3498be2a1e7fe674e02ef8c4bcf1
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-08-23 22:57:29 +0430

    add use Ref and use layoutEffect

---
+++ src/useLayoutEffect/LayoutEffectTutorial.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useLayoutEffect } from 'react'
+
+function LayoutEffectTutorial() {
+    // useLayoutEffect(() => {
+    //     console.log('Use LayoutEffect');
+    // }, [])
+    // useEffect(() => { console.log('useEffect'); }, [])
+    return (
+        <div>LayoutEffectTutorial</div>
+    )
+}
+
+export default LayoutEffectTutorial
